69d1698aa2dfebfff4bba21ff1b95bdb7db61930
Ubuntu <ubuntu@ip-172-31-25-155.ec2.internal>
2018-05-05 02:31:04 +0000

    loader changes

--- neural_srl/util/loader.py
+++ neural_srl/util/loader.py
@@ -38,7 +38,7 @@ class Loader(object):
 
         print("%i / %i / %i sentences in train / dev / test." % (len(train_data), len(dev_data), len(test_data)))
         
-        mapping_file = os.path.join(dataset,'mapping1.pkl')
+        mapping_file = os.path.join(dataset,'mapping.pkl')
         
         if not os.path.isfile(mapping_file):
             all_word_embeds = {}
